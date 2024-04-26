4d751b611cc9a8cf6a81353846418911307f7fa9
bakar-123 <113510293+bakar-123@users.noreply.github.com>
2024-04-26 08:59:54 +0530

    Update Createdclasson26042024.java 2nd update from git

--- src/main/java/prach_project/createddate26042024/Createdclasson26042024.java
+++ src/main/java/prach_project/createddate26042024/Createdclasson26042024.java
@@ -2,6 +2,14 @@ package prach_project.createddate26042024;
 
 public class Createdclasson26042024 {
 
- " ok you have created, now please add this to your code"
+ // " ok you have created, now please add this to your code"
+
+ // " May be the code i have given is not proper with syntax, let me create a method and add in that"
+
+
+ publib void method() {
+ S = " ok you have created, now please add this to your code ";
+ 
+ }
 
 }
